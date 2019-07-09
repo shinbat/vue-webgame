f01e2e5e6b99200dfda9733d5a58c9aac6d8af23
shinbat6 <shinbat6@gmail.com>
2019-07-09 13:29:07 +0900

    webpack적용

---
+++ lecture/2.word-relay/WordRelay.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div>{{word}}</div>
+            <form v-on:submit="onSubmitForm">
+                <input type="text" ref="answer" v-model="value">
+                <button type="submit">입력!</button>
+            </form>
+        <div>{{result}}</div>
+    </div>
+ </template>
+
+<script>
+export default {
+    data() {
+        return {
+            word: '제로초',
+            value: '', 
+            result: '',       
+        };
+    },
+
+    methods: {
+        onSubmitForm(e) {
+            e.preventDefault();
+            if (this.word[this.word.length - 1] === this.value[0]) {
+                this.result = '정답';
+                this.word = this.value;
+                this.value = '';
+                this.$refs.answer.focus();
+            } else {
+                this.result = '땡';
+                this.value = '';
+                this.$refs.answer.focus();
+            }
+        },
+    },
+};
+</script>
+
+<style>
+</style>
