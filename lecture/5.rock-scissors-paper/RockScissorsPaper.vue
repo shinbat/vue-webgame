1d56f27d7a057449e40e746190323a0b37c45504
shinbat6 <shinbat6@gmail.com>
2019-07-16 10:35:46 +0900

    점수추가

--- lecture/5.rock-scissors-paper/RockScissorsPaper.vue
+++ lecture/5.rock-scissors-paper/RockScissorsPaper.vue
@@ -14,9 +14,19 @@
 <script>
 const rspCoords = {
 	바위: '0',
-	가위: '-142px',
-	보: '-284px',   
+	가위: '-143px',
+	보: '-286px',   
 };
+const scores = {
+	가위: 1,
+	바위: 0,
+	보: -1,
+};
+const computerChoice = (imgCoord) => {
+	return Object.entries(rspCoords).find(function (v) {
+		return v[1] === imgCoord;
+	})[0];
+}; 
 let interval = null;
 export default {
 	data() {
@@ -26,6 +36,7 @@ export default {
 			score: 0,
 		};
 	},
+
 	computed: {
 		computedStyleObject() {
 			return {
@@ -33,11 +44,39 @@ export default {
 			};
 		},
 	},
+
 	methods: {
+		changeHand() {
+			interval = setInterval(() => {
+				if (this.imgCoord === rspCoords.바위) {
+					this.imgCoord = rspCoords.가위;
+				} else if (this.imgCoord === rspCoords.가위) {
+					this.imgCoord = rspCoords.보;
+				} else if (this.imgCoord === rspCoords.보) {
+					this.imgCoord = rspCoords.바위;
+				}
+			}, 100);			
+		},
 		onClickButton(choice) {
-			setTimeout(interval, 1000);            
+			clearInterval(interval);
+			const myScore = scores[choice];
+			const cpuScore = scores[computerChoice(this.imgCoord)];
+			const diff = myScore - cpuScore;
+			if (diff === 0) {
+				this.result = '비겼습니다';
+			} else if ([-1, 2].includes(diff)) {
+				this.result = '이겼습니다';
+				this.score += 1;
+			} else {
+				this.result = '졌습니다';
+				this.score -= 1;
+			};
+			setTimeout(() => {
+				this.changeHand();
+			}, 1000);            
 		},   
 	},
+
 	beforeCreate() {
 		console.log('beforeCreate');
 	},
@@ -49,15 +88,7 @@ export default {
 	},
 	mounted() {
 		console.log('mounted');
-		interval = setInterval(() => {
-			if (this.imgCoord === rspCoords.바위) {
-				this.imgCoord = rspCoords.가위;
-			} else if (this.imgCoord === rspCoords.가위) {
-				this.imgCoord = rspCoords.보;
-			} else if (this.imgCoord === rspCoords.보) {
-				this.imgCoord = rspCoords.바위;
-			}
-		}, 1000);
+		this.changeHand();
 	},
 	beforeUpdate() {
 		console.log('beforeUpdate');
@@ -77,7 +108,7 @@ export default {
 
 <style scoped>
 	#computer {
-		width: 142px;
+		width: 145px;
 		height: 200px;
 		background-position: 0 0;
 	}
